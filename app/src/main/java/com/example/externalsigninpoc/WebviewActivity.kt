54d2382aa99c5afd96bae1824a153abe3ef072f8
Corey Lanier <iam@coreylanier.com>
2021-03-10 00:36:23 -0500

    refactored intent to use second activity intent builder

--- app/src/main/java/com/example/externalsigninpoc/WebviewActivity.kt
+++ app/src/main/java/com/example/externalsigninpoc/WebviewActivity.kt
@@ -4,23 +4,19 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import timber.log.Timber
 
 class WebviewActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
+        Timber.d("Launched activity")
 
-    }
-
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
         val redirectUri = intent.getStringExtra(REDIRECT_URI)
         val url = intent.getStringExtra(URL)
         Timber.d("__webViewActivity | redirectUri: $redirectUri; url: $url")
-        return super.onCreateView(name, context, attrs)
     }
+
     companion object {
         const val URL = "url"
         const val REDIRECT_URI = "redirectUri"
