462cd2f4648f0da86e5fe8b2a0e102ca731b9aed
Corey Lanier <iam@coreylanier.com>
2021-03-10 16:43:43 -0500

    setting up render of FB login in webview

--- app/src/main/java/com/example/externalsigninpoc/WebviewActivity.kt
+++ app/src/main/java/com/example/externalsigninpoc/WebviewActivity.kt
@@ -1,12 +1,17 @@
 package com.example.externalsigninpoc
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import timber.log.Timber
 
 class WebviewActivity : Activity() {
+    // suppressing warning about JS enabling (maybe required for some FB login functionality)
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
@@ -15,6 +20,18 @@ class WebviewActivity : Activity() {
         val redirectUri = intent.getStringExtra(REDIRECT_URI)
         val url = intent.getStringExtra(URL)
         Timber.d("__webViewActivity | redirectUri: $redirectUri; url: $url")
+        val fbWebview = findViewById<WebView>(R.id.webview_facebook)
+        fbWebview.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+               if (url!!.startsWith(redirectUri!!)) {
+                   Timber.d("We're redirecting back to the main activity with the token")
+               }
+                return false
+            }
+        }
+
+        fbWebview.settings.javaScriptEnabled = true
+        fbWebview.loadUrl(url!!)
     }
 
     companion object {
